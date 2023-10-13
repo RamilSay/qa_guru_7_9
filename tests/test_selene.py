253b1fef627e3f3dc7c8c5e8bc69118b37c8eda3
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-10-13 21:14:09 +0700

    work

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,8 +1,8 @@
 import allure
-from selene.support.shared import browser
+from selene import browser
 
 
 def test_github():
-    with allure.step("Открываем главую страницу GitHub")
+    with allure.step("Открываем главую страницу GitHub"):
         browser.open("https://github.com")
 
